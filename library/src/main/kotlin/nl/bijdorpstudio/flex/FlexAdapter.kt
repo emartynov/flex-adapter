134be9d840123839ed140dd1efa412fed3d73a6b
Eugen Martynov <eugenemartynov@gmail.com>
2020-05-11 12:11:31 +0200

    Add Flex classes

---
+++ library/src/main/kotlin/nl/bijdorpstudio/flex/FlexAdapter.kt
@@ -0,0 +1,73 @@
+package nl.bijdorpstudio.flex
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import kotlin.properties.Delegates.observable
+
+class FlexAdapter : Adapter<FlexAdapter.FlexViewHolder>() {
+
+    internal var items: List<Item> by observable(emptyList()) { _, oldItems, newItems ->
+        onItemsChanged(
+            oldItems,
+            newItems
+        )
+    }
+
+    fun add(item: Item) {
+        val newItems = items.toMutableList()
+        newItems.add(item)
+        items = newItems
+    }
+
+    fun add(index: Int, item: Item) {
+        val newItems = items.toMutableList()
+        newItems.add(index, item)
+        items = newItems
+    }
+
+    fun clear() {
+        items = emptyList()
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun getItemViewType(position: Int) = items[position].layout
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlexViewHolder {
+        val item = items.first { it.layout == viewType }
+        val view = parent.inflate(item.layout)
+        return FlexViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FlexViewHolder, position: Int) {
+        val item = items[position]
+        item.onBind(holder.itemView as ViewGroup)
+    }
+
+    class FlexViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+    open class Item(
+        @get:LayoutRes val layout: Int
+    ) {
+
+        open fun onBind(@Suppress("UNUSED_PARAMETER") viewGroup: ViewGroup) {
+            // Override to implement.
+        }
+
+        open fun isItemTheSame(that: Item): Boolean = layout == that.layout
+
+        open fun isContentTheSame(that: Item): Boolean = isItemTheSame(that)
+    }
+
+    private fun onItemsChanged(oldItems: List<Item>, newItems: List<Item>) {
+        DiffUtil.calculateDiff(FlexDiffUtilCallback(oldItems, newItems))
+    }
+
+    private fun ViewGroup.inflate(layout: Int): View =
+        LayoutInflater.from(this.context).inflate(layout, this, false)
+}
---
+++ library/src/main/kotlin/nl/bijdorpstudio/flex/FlexRecyclerView.kt
@@ -0,0 +1,34 @@
+package nl.bijdorpstudio.flex
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class FlexRecyclerView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : RecyclerView(context, attrs, defStyleAttr) {
+
+    private val flexAdapter = FlexAdapter()
+
+    init {
+        layoutManager = LinearLayoutManager(context)
+    }
+
+    fun setItems(items: List<FlexAdapter.Item>?) {
+        flexAdapter.items = items ?: emptyList()
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        adapter = flexAdapter
+    }
+
+    override fun onDetachedFromWindow() {
+        // no memory leaks
+        swapAdapter(null, true)
+        super.onDetachedFromWindow()
+    }
+}
